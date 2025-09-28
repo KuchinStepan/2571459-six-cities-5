ff839c1066275c11f7749acae661f7ce9a926e8d
Stepan_Kuchin <stepa.ku4in@gmail.com>
2025-09-28 13:38:48 +0500

    Поправил вывод версии

--- src/cli/commands/VersionCommand.ts
+++ src/cli/commands/VersionCommand.ts
@@ -1,10 +1,14 @@
+import { createRequire } from 'node:module';
 import {ICommand} from './ICommand.js';
+
+const require = createRequire(import.meta.url);
+const pkg = require('../../../package.json');
 
 export class VersionCommand implements ICommand {
   readonly name = '--version';
   readonly description = '  --version       Print application version';
 
   invoke(): void {
-    console.log(1);
+    console.log(pkg.version);
   }
 }
